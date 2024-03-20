c552478009c719332bde148666e4b3f757abed0d
Shubham Mourya <shubham.mourya@testpoint.com.au>
2024-03-21 00:02:46 +1100

    Added setter methods
    Now user can use set methods to set VansahToken and added support for updating the url for Data Residency

---
+++ VansahNodeTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vansah;
+
+namespace Vansah_CSharp_Binding
+{
+    public class VansahNodeTests
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is from tests");
+            VansahNode vs = new VansahNode();
+
+            vs.SetVansahToken = Environment.GetEnvironmentVariable("VANSAH_TOKEN") ?? "No Value found!";
+            vs.SetVansahURL = "https://vtrunkus.vansahnode.app";
+
+            vs.JiraIssueKey = "TEST-1";
+
+
+            vs.AddQuickTestFromJiraIssue("TEST-C1","passed");            
+
+
+        }
+    }
+}
